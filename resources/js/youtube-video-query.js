d4d1cbdaba0639ab6f719d2b717db65653a1e17b
KellyPeiShan <103951774+KellyPeiShan@users.noreply.github.com>
2023-11-01 01:10:49 +0800

    done merging

--- resources/js/youtube-video-query.js
+++ resources/js/youtube-video-query.js
@@ -1,4 +1,93 @@
- // drop down list
+/* The file returns the top 12 short youtube videos regarding SQA
+ *
+ *  Date Created    : 
+ *  Date Modified   : 
+ *  License         : 
+ *  Last Author     :  
+*/
+class YoutubeAPI {
+    async loadYoutubeAPI() {
+      return new Promise((resolve, reject) => {
+          gapi.client.load('youtube', 'v3', function () {
+              console.log("Loaded youtube API");
+              resolve();
+          });
+      });
+    }
+  
+    constructor(API_KEY) {
+        this.API_KEY = API_KEY;
+        const initialize = async () => {
+          const { gapi } = window;
+          await new Promise((resolve, reject) => {
+            gapi.load('client', async () => {
+              try {
+                await gapi.client.setApiKey(this.API_KEY)
+                await this.loadYoutubeAPI();
+                await this.searchVideos();
+                resolve();
+              } catch (error) {
+                throw Error(`Error initializing gapi client: ${error}`);
+              }
+            });
+          });
+        };
+        
+        initialize();
+    }
+  
+    displayVideos(src){
+      const iframe = document.createElement("iframe");
+      iframe.src = src;
+      iframe.frameBorder = 0;
+      iframe.allowFullscreen = true;
+      iframe.allowAutoplay = true;
+      iframe.mute = true;
+      iframe.frameBorder = false;
+      iframe.showInfo = true;
+  
+      const cntYoutubePlayer = document.querySelector(".cnt-youtubeplayer");
+      cntYoutubePlayer.replaceChildren();
+      cntYoutubePlayer.appendChild(iframe); //TODO replaceChild
+      
+    }
+  
+    searchVideos(query="Software Quality Assurance") {
+      gapi.client.youtube.search.list({
+          "type": "video",
+          "videoEmbeddable": "true",
+          "videoDuration": "short",
+          "maxResults": 12,
+          "order": "rating",
+          "q": query
+      })
+        .then((response) => {
+            let src = `https://www.youtube.com/embed/VIDEO_ID?playlist=`;
+            const responseBody = JSON.parse(response.body);
+            const items = responseBody.items;
+            let video_list = "";
+            for (const item of items) {
+                video_list += item.id.videoId + ",";
+                console.log(item.body);
+            }
+            video_list = video_list.substring(0, video_list.length - 1);
+  
+            src = src + video_list + "&autoplay=1&rel=0";
+            console.log("Loading Videos for: " + src);
+  
+            this.displayVideos(src);
+        })
+        .catch((err) => { console.error("Execute error", err); });
+    }
+
+    
+  
+  }
+  
+const youtubeAPI = new YoutubeAPI("AIzaSyBRgvgjviG26TOvOimVFWfMq6dDvjJlq0o")
+  
+//keyword function
+// drop down list
  const dropdownButton = document.getElementById("dropdown-button");
  const checkboxList = document.getElementById("checkbox-list");
  const searchbar = document.getElementById("search-bar");
@@ -33,7 +122,7 @@ submitButton.addEventListener("click", function(){
     }
     query += "..."
     searchbar.value = query;
-    loadVideo(query);
+    youtubeAPI.searchVideos(query);
 })
 //when add button is click, render the checkbox with new keyword
 const addButton = document.getElementById("add-button");
@@ -55,46 +144,3 @@ addButton.addEventListener("click", function () {
     //clear input field
     document.getElementById("newkeyword-field").value="";
 });
-
-//loadvideo function, calls onload and on submit 
-function loadVideo(query = "SoftwareQualityAssurance"){
-var url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet";
-const maxResults = "12";
-const order = "rating";
-const q = query;
-const type = "video";
-const videoDuration = "short";
-const key = "AIzaSyC8MhiHwrOeQhxckyMRKVWmP1dwkNdQhdg"
-url = url + "&maxResults=" + maxResults +"&order=" + order + "&q=" + q + "&type=" + type + "&videoDuration=" +videoDuration + "&key=" + key;
-  const videoContainer = document.getElementById('video-container');
-  videoContainer.replaceChildren();
-  fetch(url).then(response => response.json())
-        .then(data => {
-
-            let videos = data.items;
-            let video_list = "";
-            let url = `https://www.youtube.com/embed/VIDEO_ID?playlist=`;
-
-            for (video of videos){
-                //get the id
-                //let videoTitle = video.snippet.title;
-                let videoId = video.id.videoId;
-
-                //append the video list
-                video_list += videoId + ",";
-            }
-
-            video_list = video_list.substring(0, video_list.length - 1);
-
-            //create the iframe and append
-            const youtubeVideo = document.createElement('iframe');
-                youtubeVideo.width = '90%';
-                youtubeVideo.height = '90%';
-                youtubeVideo.src = url + video_list;
-                youtubeVideo.allowFullscreen = true;
-            videoContainer.appendChild(youtubeVideo);    
-        })
-        .catch(error => {
-            console.error(error);
-        });
-    }
